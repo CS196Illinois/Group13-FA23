78340f4b5112a23b1f613250e4f23aa0fb4516a0
Lea <leahuangyanez@gmail.com>
2023-11-30 15:59:33 -0600

    ok

--- Project/client/src/App.js
+++ Project/client/src/App.js
@@ -3,13 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Menu from './components/menu.js';
 import Profilepg from './pages/profilepg';
 import Homepg from './pages/homepg';
+import Infopg from './pages/info';
 //import Signinbutton from './components/signin.js';
 
 function App() {
 
   return (
     <div className="App">
-     hello world
+      <Router>
+        <Routes>
+          <Route path="/" component={<Homepg />} />
+          <Route path="/profile" component={<Profilepg />} />
+          <Route path ="/info" component={<Infopg />} />
+        </Routes>
+    </Router>    
+
     </div>
   );
 }
